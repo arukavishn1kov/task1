0c5e6b54b0eb16579afa31b486331884aa46127e
arukavishn1kov <xeroxanton@gmail.com>
2024-10-08 23:59:56 +0500

    Add files via upload

--- module_1_3.py
+++ module_1_3.py
@@ -1,8 +1,8 @@
-name = "ANTON"
+name = "NAME: ANTON"
 print (name)
 age = 37
-print (age)
+print ("age:", str(age))
 new_age = age + 2
-print (new_age)
+print ("new_age:", str(new_age))
 is_student = True
-print(is_student)
+print('is_student:', str(is_student))
