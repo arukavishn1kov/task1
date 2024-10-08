9fb1584499eaf360f6133fb7ee32ef643ad68d5e
arukavishn1kov <xeroxanton@gmail.com>
2024-10-09 01:02:23 +0500

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+number_of_comp_hw_assig= 12
+hours_spent= 1.5
+course_name= 'Python'
+timeForOneTask= hours_spent/number_of_comp_hw_assig
+print('Course:', str(course_name),',', 'number of comp.hw.assig.:', str(number_of_comp_hw_assig),',', 'hours spent:', str(hours_spent),',', 'time for one task:', str(hours_spent/number_of_comp_hw_assig),'.')
